97a96291eacf4fd92859468731645c0bf085d2df
drew-loukusa <you@example.com>
2020-12-06 00:32:41 -0800

    Refactored day6

--- day6/day6.py
+++ day6/day6.py
@@ -1,5 +1,5 @@
 from aocd.models import Puzzle
-from collections import defaultdict
+from collections import defaultdict as dd
 def save_input_to_file(puzzle, day):
     filename = "day" + str(day) + ".txt"
     with open(filename, mode='w+') as f:
@@ -18,53 +18,42 @@ lines = [line.rstrip() for line in open(infile)]
 
 # Part 1
 if False:
-    sum_count = 0
+    asum = 0
     cur_set = set()
     for line in lines: 
         for c in line: 
-            #print(f"Adding {c} to cur set")
             cur_set.add(c)
         
         if len(line) == 0:
-            #print(f"Found {len(cur_set)} in set ")
-            sum_count += len(cur_set)
+            asum += len(cur_set)
             cur_set = set()
-            
-  
-   # print(f"Found {len(cur_set)} in set ")
-    sum_count += len(cur_set)
-    cur_set = set()
-            
-    #puzzle.answer_a = sum_count
-    print(sum_count)
+
+    asum += len(cur_set)
+    print(asum)
+    #puzzle.answer_a = asum
+
 # Part 2
 
+p2Answer = 3158
+
+def common_in(group):
+    common_qs = set("abcdefghijklmnopqrstuvwxqyz")
+    for k,v in group.items():
+        common_qs = common_qs & v
+    return len(common_qs)
+
 if True:
-    sum_count = 0
-    personID = 0
-    cur_group = defaultdict(set)
+    asum, i, group = 0, 0, dd(set)
     for line in lines: 
         for c in line: 
-            #print(f"Adding {c} to cur set")
-            cur_group[personID].add(c)
-        personID += 1
+            group[i].add(c)
+        i += 1
 
         if len(line) == 0:
-            common_qs = set("abcdefghijklmnopqrstuvwxqyz")
-            for k,v in cur_group.items():
-                common_qs = common_qs & v
-            sum_count += len(common_qs)
-            cur_group = defaultdict(set)
-            personID = 0
+            asum += common_in(group)
+            group, i = dd(set), 0
 
-   # print(f"Found {len(cur_set)} in set ")
-    common_qs = set("abcdefghijklmnopqrstuvwxqyz")
-    for k,v in cur_group.items():
-        common_qs = common_qs & v
-    sum_count += len(common_qs)
-    cur_group = defaultdict(set)
-    personID = 0
-    
-    print(sum_count)
+    asum += common_in(group)
+    print(asum)
 
-    #puzzle.answer_b = sum_count
+    #puzzle.answer_b = asum
