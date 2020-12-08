3b2b1d58d9bc8f4ee771a507ad8cab3474aa4e8e
drew-loukusa <you@example.com>
2020-12-07 21:21:24 -0800

    day8 part 1

--- day8/day8.py
+++ day8/day8.py
@@ -9,21 +9,54 @@ def save_input_to_file(puzzle, day):
 Year=2020
 Day=8
 
-puzzle = Puzzle(year=Year, day=Day)
-save_input_to_file(puzzle, day=Day)
+# puzzle = Puzzle(year=Year, day=Day)
+# save_input_to_file(puzzle, day=Day)
 
 infile = "day8ex.txt"
 infile = "day8.txt"
 lines = [line.rstrip() for line in open(infile)]
 
 # Part 1
-if True:
-    for line in lines: 
-        pass
+if False:
+    i = 0
+    accumulator = 0
+    processed_instrs = set()
+    while i < len(lines):
+        
+        if lines[i] + str(i) in processed_instrs:
+            break 
+        else:
+            processed_instrs.add(lines[i]+str(i))
+
+        tokens = lines[i].replace('+','').split(' ')
+        instr, offset = tokens[0], int(tokens[1])
+
+        # print("cur intstr and offset:", instr, offset)
+        # print("accumulator", accumulator)
+
+        if instr == 'acc': 
+            accumulator += offset
+            i += 1
+
+        elif instr == 'jmp': 
+            i += offset
+
+        elif instr == 'nop': 
+            i += 1
+            continue 
+
+        # print("AFTER PROCESSING INSTR:")
+        # print("cur intstr and offset:", instr, offset)
+        # print("accumulator", accumulator)
+        # input(">>>")
+
+    print(accumulator)
+
     #puzzle.answer_a = result
 
 # Part 2
 if True:
+
     pass
 
     #puzzle.answer_b = result
