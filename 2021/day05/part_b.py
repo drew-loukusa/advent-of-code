656df0d4cca78f4f6720cc70c97b53f4cde346bb
drew-loukusa <dlwerd@gmail.com>
2021-12-04 22:37:08 -0800

    Day 5 part 2

--- 2021/day05/part_b.py
+++ 2021/day05/part_b.py
@@ -1,20 +1,65 @@
 # https://adventofcode.com/2021/day/5
 
 import sys
+from collections import defaultdict as dd
 from aocd.models import Puzzle
 from my_aoc_utils.utils import save_puzzle, AOC_Test
+from part_a import process, Point
 
-def process(infile):
-    """Process the input file into a data structure for solve()"""
-    return [line.rstrip() for line in open(infile)]
+def count_overlaps(data, OVERLAP_NUM=2):
+    overlaps = set()
+    points = dd(int)
+    for a,b in data:
+        
+        # Generate points in between a and b 
+        line_points = []
 
-def solve(data):
-    result = None 
-    # Problem soving go HERE
-    return result 
+        # Vertical line 
+        if a.x == b.x:
+            start = a if a.y < b.y else b
+            end = a if a.y > b.y else b
+            line_points.append(start)
+            for y in range(start.y + 1, end.y):
+                line_points.append(Point(start.x, y))
+            line_points.append(end)
+
+        # Horizontal line 
+        elif a.y == b.y:
+            start = a if a.x < b.x else b
+            end = a if a.x > b.x else b 
+            line_points.append(start)
+            for x in range(start.x + 1, end.x):
+                line_points.append(Point(x, start.y))
+            line_points.append(end)
+
+        # Diagonal line
+        else:
+            start = a if a.x < b.x else b 
+            end = a if a.x > b.x else b 
+            line_points.append(start)
+            y = start.y 
+            for x in range(start.x + 1, end.x):
+                if start.y > end.y:
+                    y -= 1
+                else:
+                    y += 1
+                line_points.append(Point(x, y))
+            line_points.append(end)
+
+        for point in line_points:
+            points[(point.x, point.y)] += 1
+
+            # Check after incrementing a point if you have walked
+            # over it already. If yes, add it to the overlaps set
+            if points[(point.x, point.y)] >= OVERLAP_NUM:
+                overlaps.add((point.x, point.y))
+
+        #dump_points(points)
+
+    return len(overlaps)
 
 def main(infile):
-    return solve(process(infile))
+    return count_overlaps(process(infile))
     
 if __name__ == "__main__":
     year, day = 2021, 5
@@ -22,10 +67,10 @@ if __name__ == "__main__":
     aoc = AOC_Test(main, __file__)
 
     # TESTS, test against example input, other test input here
-    aoc.test("day5ex.txt", ans=None)
+    aoc.test("day5ex.txt", ans=12)
 
     # Run question 
-    aoc.test("day5.txt", ans=None, save_answer=True)
+    aoc.test("day5.txt", ans=-1, save_answer=True)
 
     # Submit if user passed in 'submit' on command line
     if len(sys.argv) > 1 and sys.argv[1] == "submit":
