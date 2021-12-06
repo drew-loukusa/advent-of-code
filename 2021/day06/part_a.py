6677fcbc185401a96f54f208540d2abb8c874acd
drew-loukusa <dlwerd@gmail.com>
2021-12-05 23:05:44 -0800

    Day 6 part 1

--- 2021/day06/part_a.py
+++ 2021/day06/part_a.py
@@ -4,14 +4,26 @@ import sys
 from aocd.models import Puzzle
 from my_aoc_utils.utils import save_puzzle, AOC_Test
 
+DAYS_TO_RUN = 80
+CYCLE_LENGTH = 6 # 0 is included as a valid value, so this wolud be 7 
+INITIAL_CYCLE_LENGTH = 8 # ditto, would be 9
+
 def process(infile):
     """Process the input file into a data structure for solve()"""
-    return [line.rstrip() for line in open(infile)]
+    return [int(n) for n in open(infile).readline().split(',')]
 
-def solve(data):
-    result = None 
-    # Problem soving go HERE
-    return result 
+def solve(school: list):
+    # Naive brute force solution
+    for _ in range(DAYS_TO_RUN):
+        new_fishes = []
+        for i, fish in enumerate(school):
+            if fish == 0:
+                new_fishes.append(INITIAL_CYCLE_LENGTH)
+                school[i] = CYCLE_LENGTH
+            else:
+                school[i] -= 1
+        school.extend(new_fishes)
+    return len(school)
 
 def main(infile):
     return solve(process(infile))
@@ -22,10 +34,10 @@ if __name__ == "__main__":
     aoc = AOC_Test(main, __file__)
 
     # TESTS, test against example input, other test input here
-    aoc.test("day6ex.txt", ans=None)
+    aoc.test("day6ex.txt", ans=5934)
 
     # Run question 
-    aoc.test("day6.txt", ans=None, save_answer=True)
+    aoc.test("day6.txt", ans=-1, save_answer=True)
 
     # Submit if user passed in 'submit' on command line
     if len(sys.argv) > 1 and sys.argv[1] == "submit":
