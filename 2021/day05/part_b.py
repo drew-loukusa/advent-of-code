bb902b1dcb9ce289b1b097d43a716b4071a104fc
drew-loukusa <dlwerd@gmail.com>
2021-12-05 16:02:29 -0800

    Improve algorithm for day 5 part 2

--- 2021/day05/part_b.py
+++ 2021/day05/part_b.py
@@ -2,7 +2,7 @@
 
 import sys
 from collections import defaultdict as dd
-from typing import List
+from typing import List, overload
 from aocd.models import Puzzle
 from my_aoc_utils.utils import save_puzzle, AOC_Test
 from part_a import Point
@@ -68,8 +68,89 @@ def count_overlaps(segments: List[List[Point]], overlap_minimum=2):
 
     return len(overlaps)
 
+
+
+#============================================================================#
+#                           Improved algorithm                               #
+#============================================================================#
+
+# Count the number of times you walk on a given point in a line segment WHILE 
+# you generate said line segment. 
+# Previously I was generating the segments all at once, then walking over them
+# in a seperate step. 
+# I walk over them to count each point, but I can do that WHEN I MAKE the segment.
+
+def read_pairs(infile):
+    """Generator function that gets the next pair coords defining a line segment"""
+    for line in open(infile):
+        line = line.rstrip()     
+        a, _, b = line.split()
+        a = Point(*a.split(','))
+        b = Point(*b.split(','))
+        yield (a,b)
+
+def walk_segment(a: Point, b: Point, points: dict, overlaps: set):
+    segment = []
+    # Vertical line 
+    if a.x == b.x:
+        start = a if a.y < b.y else b
+        end = a if a.y > b.y else b
+        for y in range(start.y, end.y + 1):
+            point = Point(start.x, y)
+            segment.append(point)
+            points[(point.x, point.y)] += 1
+
+            # Check after incrementing a point if you have walked
+            # over it already. If yes, add it to the overlaps set
+            if points[(point.x, point.y)] >= 2:
+                overlaps.add((point.x, point.y))
+        
+
+    # Horizontal line 
+    elif a.y == b.y:
+        start = a if a.x < b.x else b
+        end = a if a.x > b.x else b 
+        for x in range(start.x, end.x + 1):
+            point = Point(x, start.y)
+            segment.append(point)
+            points[(point.x, point.y)] += 1
+
+            # Check after incrementing a point if you have walked
+            # over it already. If yes, add it to the overlaps set
+            if points[(point.x, point.y)] >= 2:
+                overlaps.add((point.x, point.y))
+
+
+    # Diagonal line
+    else:
+        start = a if a.x < b.x else b 
+        end = a if a.x > b.x else b 
+        y = start.y 
+        for x in range(start.x, end.x + 1):
+            point = Point(x, y)
+            segment.append(point)
+            if start.y > end.y:
+                y -= 1
+            else:
+                y += 1
+            points[(point.x, point.y)] += 1
+
+            # Check after incrementing a point if you have walked
+            # over it already. If yes, add it to the overlaps set
+            if points[(point.x, point.y)] >= 2:
+                overlaps.add((point.x, point.y))
+
+def count_overlaps_while_generating_segments(infile):
+    points = dd(int)
+    overlap = set()
+    coord_pairs = read_pairs(infile)
+    for a,b in coord_pairs:
+        walk_segment(a, b, points, overlap)
+    return len(overlap)
+
 def main(infile):
-    return count_overlaps(generate_segments(infile))
+    # return count_overlaps(generate_segments(infile))
+    return count_overlaps_while_generating_segments(infile)
     
 if __name__ == "__main__":
     year, day = 2021, 5
