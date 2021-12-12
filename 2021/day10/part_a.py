5cd356cb01ff90b4e2e0cfd9cab7ba2cdb4b5367
drew-loukusa <dlwerd@gmail.com>
2021-12-11 16:52:00 -0800

    Day 10 p2

--- 2021/day10/part_a.py
+++ 2021/day10/part_a.py
@@ -12,29 +12,29 @@ def process(infile):
 def solve(data):
     result = None 
     open_chars = set("([{<")
-    br = {')':'(', ']':'[', '}':'{', '>':'<'}
+    close_to_open = {')':'(', ']':'[', '}':'{', '>':'<'}
     stack = []
-    corrupted_chars = dd(int)
+    bad_char_count = dd(int)
     for line in data:
-        corrupted_char = None 
+        bad_char = None 
         for char in line:
             if char in open_chars:
                 stack.append(char)
                 continue 
             if char not in open_chars:
-                matching_open_char = br[char]
-                if matching_open_char == stack[-1]:
+                open_char = close_to_open[char]
+                if open_char == stack[-1]:
                     stack.pop()
                 else:
-                    corrupted_char = char 
+                    bad_char = char 
                     break 
-        if corrupted_char != None:
-            corrupted_chars[corrupted_char] += 1
+        if bad_char != None:
+            bad_char_count[bad_char] += 1
     
     scores = {')':3, ']': 57, '}':1197, '>':25137}
     syntax_error_score = 0
-    for char in corrupted_chars:
-        syntax_error_score += scores[char] * corrupted_chars[char]
+    for char in bad_char_count:
+        syntax_error_score += scores[char] * bad_char_count[char]
     
     return syntax_error_score
 
@@ -50,7 +50,7 @@ if __name__ == "__main__":
     aoc.test("day10ex.txt", ans=26397)
 
     # Run question 
-    aoc.test("day10.txt", ans=-1, save_answer=True)
+    aoc.test("day10.txt", ans=316851, save_answer=True)
 
     # Submit if user passed in 'submit' on command line
     if len(sys.argv) > 1 and sys.argv[1] == "submit":
--- 2021/day10/part_b.py
+++ 2021/day10/part_b.py
@@ -1,17 +1,52 @@
 # https://adventofcode.com/2021/day/10
 
 import sys
+from collections import defaultdict as dd
 from aocd.models import Puzzle
 from my_aoc_utils.utils import save_puzzle, AOC_Test
+
+OPEN_CHARS = set("([{<")
+CLOSE_TO_OPEN = {')':'(', ']':'[', '}':'{', '>':'<'}
+OPEN_TO_CLOSE = {v:k for k,v in CLOSE_TO_OPEN.items()}
 
 def process(infile):
     """Process the input file into a data structure for solve()"""
     return [line.rstrip() for line in open(infile)]
 
+def process_line(line) -> list:
+    stack = []
+    for char in line:
+        if char in OPEN_CHARS:
+            stack.append(char)
+            continue 
+        if char not in OPEN_CHARS:
+            open_char = CLOSE_TO_OPEN[char]
+            if open_char == stack[-1]:
+                stack.pop()
+            # If the line is corrupted, return an empty list
+            else:
+                return None
+    return stack 
+
 def solve(data):
-    result = None 
-    # Problem soving go HERE
-    return result 
+    scores = []
+    points = {')':1, ']':2, '}':3, '>':4}
+    for line in data:
+        stack = process_line(line)
+        # Skip corrupted lines
+        if stack is None: continue
+        # Calculate what is needed to complete the line:
+        print(end='')
+        line_score = 0
+        while len(stack) > 0:
+            closeing_char = OPEN_TO_CLOSE[stack.pop()]
+            line_score = line_score * 5 + points[closeing_char]
+        scores.append(line_score)
+    
+    length = len(scores)
+    index = length//2
+    sorted_scores = sorted(scores)
+    return sorted_scores[index]
 
 def main(infile):
     return solve(process(infile))
@@ -22,10 +57,10 @@ if __name__ == "__main__":
     aoc = AOC_Test(main, __file__)
 
     # TESTS, test against example input, other test input here
-    aoc.test("day10ex.txt", ans=None)
+    aoc.test("day10ex.txt", ans=288957)
 
     # Run question 
-    aoc.test("day10.txt", ans=None, save_answer=True)
+    aoc.test("day10.txt", ans=2182912364, save_answer=True)
 
     # Submit if user passed in 'submit' on command line
     if len(sys.argv) > 1 and sys.argv[1] == "submit":
